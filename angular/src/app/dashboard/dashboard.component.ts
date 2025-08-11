0deca909c0531e4ecae4747a9e2c540284da45d6
keegan.smith <keegan.smith@entelect.co.za>
2025-08-11 16:48:26 +0200

    Inital commit

---
+++ angular/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AppModule } from '../app.module';
+import { HeroSearchComponent } from '../hero-search/hero-search.component';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports:[CommonModule, RouterModule, HeroSearchComponent],
+  templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.css' ]
+})
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+  }
+}
